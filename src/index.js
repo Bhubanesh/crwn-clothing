a718423d2697d7cfcb3213a7f02d6442ba53204e
Bhubanesh Dwibedi <bhubanesh1994@gmail.com>
2023-06-22 00:32:59 +0530

    creating a branch for product card

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { UserProvider } from "./context/user.context";
 import App from "./App";
+import { UserProvider } from "./context/user.context";
+import { ProductsProvider } from "./context/products.context";
 
 import "./index.scss";
 
@@ -14,7 +15,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
