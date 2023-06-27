e4e69617aff280e616c9a45cc8dbfaf1efde4f7d
Bhubanesh Dwibedi <bhubanesh1994@gmail.com>
2023-06-27 12:36:43 +0530

    added the add to cart feature

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,34 +1,29 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from '../utils/firebase/firebase.utils';
 
-//the actual value you want to access
 export const UserContext = createContext({
-  currentUser: null,
   setCurrentUser: () => null,
+  currentUser: null,
 });
 
-//this is the actual component
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null); // the initial value of currentValue is in the useState, we need to set a default value for it in the createContext as an object
+  const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
-    const unsuscribe = onAuthStateChangedListener((user) => {
-      if(user){
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
         createUserDocumentFromAuth(user);
       }
       setCurrentUser(user);
     });
-    return unsuscribe;
+
+    return unsubscribe;
   }, []);
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
-
-
-  /* <userContext.Provider><app /></userContext.Provider> */
- //here app is the children
-
-// the value holds the actual contextual value
-
-// the userProvider is allowing the child component to access the data inside the useState
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,16 @@
-import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-import { CartContext } from "../../contexts/cart.context";
-import { UserContext } from "../../contexts/user.context";
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
-import { SignOutUser } from "../../utils/firebase/firebase.utils";
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import "./navgation.styles.scss"
-
+import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -19,20 +18,21 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
-          <CrwnLogo className="logo" />
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/Shop">
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
+
           {currentUser ? (
-            <span className="nav-link" onClick={SignOutUser}>
+            <span className='nav-link' onClick={signOutUser}>
               SIGN OUT
             </span>
           ) : (
-            <Link className="nav-link" to="/auth">
+            <Link className='nav-link' to='/auth'>
               SIGN IN
             </Link>
           )}
