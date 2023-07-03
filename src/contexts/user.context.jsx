787c92920ac84841b4a8a97a929074e9cdaee379
Bhubanesh Dwibedi <bhubanesh1994@gmail.com>
2023-07-04 02:00:17 +0530

    enhanced the cart contex with reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,45 +1,41 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useEffect, useReducer } from 'react';
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-} from "../utils/firebase/firebase.utils";
+} from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
   currentUser: null,
 });
 
-export const USER_ACTION_TYPE = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
-
-const userReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPE.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      };
-    default:
-      throw new Error(`Unhandled type ${type} is unhandled`);
-  }
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
 };
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
-  const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPE.SET_CURRENT_USER, payload: user });
-  };
-
-  const value = { currentUser, setCurrentUser };
+  const setCurrentUser = (user) =>
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
@@ -51,6 +47,12 @@ export const UserProvider = ({ children }) => {
 
     return unsubscribe;
   }, []);
+
+  console.log(currentUser);
+
+  const value = {
+    currentUser,
+  };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
